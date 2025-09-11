c670d4ed0052e17f674a91e81ebeaf71b2a58aa0
criki76 <109381712+criki76@users.noreply.github.com>
2025-09-11 20:15:42 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,8 @@
-// Your web app's Firebase configuration
-const firebaseConfig = {
-    apiKey: "AIzaSyDEYxuxC_snlko4T3E-WQlD7VkYoLlOIOw",
-    authDomain: "o-r-m-a.firebaseapp.com",
-    projectId: "o-r-m-a",
-    storageBucket: "o-r-m-a.firebasestorage.app",
-    messagingSenderId: "832487381320",
-    appId: "1:832487381320:web:1237a1a488fb44dfd9bb7d"
+export const firebaseConfig = {
+  apiKey: "LA_TUA_API_KEY",
+  authDomain: "il-tuo-progetto.firebaseapp.com",
+  projectId: "il-tuo-progetto",
+  storageBucket: "il-tuo-progetto.appspot.com",
+  messagingSenderId: "NUMERO",
+  appId: "LA_TUA_APP_ID"
 };
